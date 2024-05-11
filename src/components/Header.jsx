13f0d8d596c0d45c0e0ce8b749cd0152c380ea64
Walid-Ansari <walidwasim26@gmail.com>
2024-05-11 21:28:11 +0530

    Header Section Completed

---
+++ src/components/Header.jsx
@@ -0,0 +1,83 @@
+import { brainwave } from "../assets";
+import { navigation } from "../constants/index";
+import { useLocation } from "react-router-dom";
+import { HambugerMenu } from "./design/Header";
+import { useState } from "react";
+import { disablePageScroll, enablePageScroll } from "scroll-lock";
+
+import MenuSvg from "../assets/svg/MenuSvg";
+import Button from "./Button";
+const Header = () => {
+  const [openNavigation, setOpenNavigation] = useState(false);
+  const pathname = useLocation();
+
+  const toggleMenu = () => {
+    if (openNavigation) {
+      setOpenNavigation(false);
+      enablePageScroll();
+    } else {
+      setOpenNavigation(true);
+      disablePageScroll();
+    }
+  };
+
+  const handleClick = () => {
+    if (!openNavigation) return;
+
+    enablePageScroll();
+    setOpenNavigation(false);
+  };
+
+  return (
+    <div
+      className={`fixed top-0  left-0 w-full z-50  border-b border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm ${
+        openNavigation ? "bg-n-8" : "bg-n-8/90 backdrop-blur-sm"
+      }`}
+    >
+      <div className="flex items-center px-5 lg:px-7.5 xl:px-10 max-lg:py-4">
+        <a className="block w-[12rem] xl:mr-8" href="#hero">
+          <img src={brainwave} width={190} height={40} alt="brainwave" />
+        </a>
+        <nav
+          className={`${
+            openNavigation ? "flex" : "hidden"
+          } fixed  top-[5rem] left-0 right-0 bottom-0 bg-n-8 lg:static lg:flex lg:mx-auto lg:bg-transparent`}
+        >
+          <div className="relative z-2 flex flex-col items-center justify-center m-auto lg:flex-row">
+            {navigation.map((nav) => (
+              <a
+                key={nav.id}
+                href={nav.url}
+                onClick={handleClick}
+                className={`block relative font-code text-2xl uppercase text-n-1 transition-colors hover:text-color-1 ${
+                  nav.onlyMobile ? "lg:hidden" : ""
+                } px-6 py-6 md:py-8 lg:-mr-[0.25] lg:text-sm lg:font-semibold ${
+                  nav.url === pathname.hash ? "z-2 lg:text-n-1" : "text-n-1/50"
+                } lg:leading-5 lg:hover:text-n-1 xl:px-10`}
+              >
+                {nav.title}
+              </a>
+            ))}
+          </div>
+          <HambugerMenu />
+        </nav>
+
+        <a
+          href="#signup"
+          className="button hidden mr-8 text-n-1/50 transition-colors hover:text-n-1
+        lg:block"
+        >
+          New Account
+        </a>
+        <Button className="hidden lg:flex" href="#login">
+          Sign In{" "}
+        </Button>
+        <Button className="ml-auto lg:hidden px-3" onClick={toggleMenu}>
+          <MenuSvg openNavigation={openNavigation} />
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
