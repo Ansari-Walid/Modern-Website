9855342fe05333b4637271273a19766d10865284
Walid-Ansari <walidwasim26@gmail.com>
2024-05-13 15:19:46 +0530

    Hero section completedp

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import ButtonGradient from "./assets/svg/ButtonGradient";
-import Button from "./components/Button";
 import Header from "./components/Header";
-
+import Hero from "./components/Hero";
 function App() {
   return (
     <>
       <h1 className="text-3xl font-bold underline">Hello World</h1>
       <div className="pt-[4.75rem] lg:pt[5.25rem] overflow-hidden">
         <Header />
+        <Hero />
       </div>
       <ButtonGradient />
     </>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -30,7 +30,7 @@ const Header = () => {
 
   return (
     <div
-      className={`fixed top-0  left-0 w-full z-50  border-b border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm ${
+      className={`fixed top-0 left-0 w-full z-50  border-b border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm ${
         openNavigation ? "bg-n-8" : "bg-n-8/90 backdrop-blur-sm"
       }`}
     >
@@ -38,6 +38,7 @@ const Header = () => {
         <a className="block w-[12rem] xl:mr-8" href="#hero">
           <img src={brainwave} width={190} height={40} alt="brainwave" />
         </a>
+
         <nav
           className={`${
             openNavigation ? "flex" : "hidden"
@@ -51,8 +52,10 @@ const Header = () => {
                 onClick={handleClick}
                 className={`block relative font-code text-2xl uppercase text-n-1 transition-colors hover:text-color-1 ${
                   nav.onlyMobile ? "lg:hidden" : ""
-                } px-6 py-6 md:py-8 lg:-mr-[0.25] lg:text-sm lg:font-semibold ${
-                  nav.url === pathname.hash ? "z-2 lg:text-n-1" : "text-n-1/50"
+                } px-6 py-6 md:py-8 lg:-mr-0.25 lg:text-sm lg:font-semibold ${
+                  nav.url === pathname.hash
+                    ? "z-2 lg:text-n-1"
+                    : "lg:text-n-1/50"
                 } lg:leading-5 lg:hover:text-n-1 xl:px-10`}
               >
                 {nav.title}
@@ -64,15 +67,14 @@ const Header = () => {
 
         <a
           href="#signup"
-          className="button hidden mr-8 text-n-1/50 transition-colors hover:text-n-1
-        lg:block"
+          className="button hidden mr-8 text-n-1/50 transition-colors hover:text-n-1 lg:block"
         >
           New Account
         </a>
         <Button className="hidden lg:flex" href="#login">
           Sign In{" "}
         </Button>
-        <Button className="ml-auto lg:hidden px-3" onClick={toggleMenu}>
+        <Button className="ml-auto lg:hidden" px="px-3" onClick={toggleMenu}>
           <MenuSvg openNavigation={openNavigation} />
         </Button>
       </div>
