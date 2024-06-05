ff4d8c5f6d496e79cd2d84ffc71c053f4345efa5
Walid-Ansari <walidwasim26@gmail.com>
2024-06-05 15:33:05 +0530

    deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,24 @@
 import ButtonGradient from "./assets/svg/ButtonGradient";
 import Benifits from "./components/Benifits";
 import Collaboration from "./components/Collaboration";
+import { Footer } from "./components/Footer";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import Pricing from "./components/Pricing";
+import Roadmap from "./components/Roadmap";
 import Services from "./components/Services";
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello World</h1>
       <div className="pt-[4.75rem] lg:pt[5.25rem] overflow-hidden">
         <Header />
         <Hero />
         <Benifits />
         <Collaboration />
         <Services />
+        <Pricing />
+        <Roadmap />
+        <Footer />
       </div>
       <ButtonGradient />
     </>
